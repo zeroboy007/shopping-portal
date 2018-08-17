9b0076388be0ddbcb669e93fddc0c67bb7d35634
Gourav Shah <gs@initcron.org>
2018-08-17 10:31:06 +0530

    reverted endpoints
    reverted endpoints to original configs to make assertions work

--- api/endpoints.js
+++ api/endpoints.js
@@ -15,9 +15,9 @@
   });
 
   module.exports = {
-    catalogueUrl:  util.format("http://catalogue:7081%s", domain),
+    catalogueUrl:  util.format("http://catalogue%s", domain),
     tagsUrl:       util.format("http://catalogue%s/tags", domain),
-    cartsUrl:      util.format("http://carts:8081%s/carts", domain),
+    cartsUrl:      util.format("http://carts%s/carts", domain),
     ordersUrl:     util.format("http://orders%s", domain),
     customersUrl:  util.format("http://user%s/customers", domain),
     addressUrl:    util.format("http://user%s/addresses", domain),
