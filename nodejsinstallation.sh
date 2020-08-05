490addb1b3748638ffde690ddecc8a133315255a
GitHub Developer <deraviyam@gmail.com>
2020-08-05 16:20:56 +0530

    Update nodejsinstallation.sh

--- nodejsinstallation.sh
+++ nodejsinstallation.sh
@@ -1,28 +1,19 @@
-#!/bin/bash 
+For the recent versions of Alpine (v3.8+) the correct way to install nodejs with npm is:
 
-export NODE_VERSION=4.8.6
-
-apk add --no-cache libstdc++ 
-apk add --no-cache --virtual .build-deps \
-        binutils-gold \
-        curl \
-        g++ \
-        gcc \
-        gnupg \
-        libgcc \
-        linux-headers \
-        make \
-        python 
-
-curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz" 
-tar -xf "node-v$NODE_VERSION.tar.xz" 
-cd "node-v$NODE_VERSION" 
-./configure 
-make -j$(getconf _NPROCESSORS_ONLN) 
-make install 
-apk del .build-deps 
-cd .. \
-rm -rf node-v$NODE_VERSION.tar.xz node-v$NODE_VERSION
+apk add --update nodejs npm
 
 
-exit 0 
+
+
+
+
+
+
+
+
+
+
+#############If and only if required#########################
+However, npm package depends on nodejs, so you can do:
+
+apk add --update npm
